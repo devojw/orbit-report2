7d37e709e5c971d5979171bbd5fed78d60c2732c
myooi <myooi@W3L-CYB-M-WK01.local>
2020-12-22 09:57:56 -0600

    Created Orbit List Component

---
+++ orbit-report2/src/app/orbit-list/orbit-list.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { OrbitListComponent } from './orbit-list.component';
+
+describe('OrbitListComponent', () => {
+  let component: OrbitListComponent;
+  let fixture: ComponentFixture<OrbitListComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ OrbitListComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(OrbitListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ orbit-report2/src/app/orbit-list/orbit-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-orbit-list',
+  templateUrl: './orbit-list.component.html',
+  styleUrls: ['./orbit-list.component.css']
+})
+export class OrbitListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
