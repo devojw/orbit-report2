8e2307ff397ff36dd8660dba08d398ae19d56ded
myooi <myooi@W3L-CYB-M-WK01.local>
2020-12-22 10:25:20 -0600

    Fetched Satellite Data

--- orbit-report2/src/app/app.component.ts
+++ orbit-report2/src/app/app.component.ts
@@ -10,12 +10,20 @@ export class AppComponent {
   title = 'orbit-report2';
   sourceList: Satellite[];
   constructor() {
-    this.sourceList = [
-       new Satellite("SiriusXM", "Communication", "2009-03-21", "LOW", true),
-       new Satellite("Cat Scanner", "Imaging", "2012-01-05", "LOW", true),
-       new Satellite("Weber Grill", "Space Debris", "1996-03-25", "HIGH", false),
-       new Satellite("GPS 938", "Positioning", "2001-11-01", "HIGH", true),
-       new Satellite("ISS", "Space Station", "1998-11-20", "LOW", true),
-    ];
+    this.sourceList = [];
+    let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+ 
+    window.fetch(satellitesUrl).then(function(response) {
+       response.json().then(function(data) {
+ 
+          let fetchedSatellites = data.satellites;
+          for(let i =0; i <fetchedSatellites.length; i++){
+            let satellite = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
+            this.sourceList.push(satellite);
+          }
+ 
+       }.bind(this));
+    }.bind(this));
+ 
  }
 }
