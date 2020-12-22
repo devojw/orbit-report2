737f717404e765932c8218f935ef0bbaa56adabf
myooi <myooi@W3L-CYB-M-WK01.local>
2020-12-22 11:09:56 -0600

    Added Search Feature

--- orbit-report2/src/app/app.component.ts
+++ orbit-report2/src/app/app.component.ts
@@ -9,8 +9,10 @@ import { Satellite } from './satellite';
 export class AppComponent {
   title = 'orbit-report2';
   sourceList: Satellite[];
+  displayList: Satellite[];
   constructor() {
     this.sourceList = [];
+    this.displayList = []
     let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
  
     window.fetch(satellitesUrl).then(function(response) {
@@ -20,10 +22,23 @@ export class AppComponent {
           for(let i =0; i <fetchedSatellites.length; i++){
             let satellite = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
             this.sourceList.push(satellite);
-          }
- 
+          }// make a copy of the sourceList to be shown to the user
+      this.displayList = this.sourceList.slice(0);
        }.bind(this));
     }.bind(this));
  
  }
+ search(searchTerm: string): void {
+  let matchingSatellites: Satellite[] = [];
+  searchTerm = searchTerm.toLowerCase();
+  for(let i=0; i < this.sourceList.length; i++) {
+     let name = this.sourceList[i].name.toLowerCase();
+     if (name.indexOf(searchTerm) >= 0) {
+        matchingSatellites.push(this.sourceList[i]);
+     }
+  }
+  // assign this.displayList to be the array of matching satellites
+  // this will cause Angular to re-make the table, but now only containing matches
+  this.displayList = matchingSatellites;
+}
 }
